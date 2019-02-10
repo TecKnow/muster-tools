206762a9fe508fca3e7ed2726048ab6ee3b2ff91
David P. Perkins <tecknow@grumbleware.com>
2019-02-09 20:53:03 -0600

    Added react-beautiful-dnd to project, wrapped application in required context.
    The context should probably be broken into its own file, but I wanted to set up the basics as quickly as possible.

--- muster_react/src/App.js
+++ muster_react/src/App.js
@@ -3,6 +3,7 @@ import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import blue from "@material-ui/core/colors/blue";
 import orange from "@material-ui/core/colors/orange";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { DragDropContext } from "react-beautiful-dnd";
 import "typeface-roboto";
 import { Button } from "@material-ui/core";
 import "./App.css";
@@ -18,13 +19,31 @@ const theme = createMuiTheme({
 });
 
 class App extends Component {
+  // https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/guides/responders.md
+
+  onDragStart = start => {
+    return;
+  };
+  onDragUpdate = () => {
+    return;
+  };
+  onDragEnd = () => {
+    // the only one that is required
+    return;
+  };
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+        <DragDropContext
+          onDragStart={this.onDragStart}
+          onDragUpdate={this.onDragUpdate}
+          onDragEnd={this.onDragEnd}
+        >
+          <CssBaseline />
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+        </DragDropContext>
       </MuiThemeProvider>
     );
   }
