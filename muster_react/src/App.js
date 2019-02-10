36431ab936bbc9669e6d90db73d1213aa8ea463d
David P. Perkins <tecknow@grumbleware.com>
2019-02-09 20:25:37 -0600

    Pin a working version of ajv.
    The latest version of ajv, a schema parser that create-react-app relies on, has some incomparability with webpack that causes 'npm start' to fail.  The solution appears to be to pin a previous version, so that's what I've done.
    
    This leads to some conflicts with wanted versions in NPM that will need to be monitored, but for now this is what allows work to move forward.

--- muster_react/src/App.js
+++ muster_react/src/App.js
@@ -4,6 +4,7 @@ import blue from "@material-ui/core/colors/blue";
 import orange from "@material-ui/core/colors/orange";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "typeface-roboto";
+import { Button } from "@material-ui/core";
 import "./App.css";
 
 const theme = createMuiTheme({
@@ -21,6 +22,9 @@ class App extends Component {
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </MuiThemeProvider>
     );
   }
