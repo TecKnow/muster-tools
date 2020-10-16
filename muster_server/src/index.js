e13f8992d9622036359b916a180cf30e490ba54b
David P. Perkins <tecknow@grumbleware.com>
2020-10-16 18:19:34 -0500

    Add redux-socket.io to the server
    This also adds socket.io itself to the dependencies.
    
    server configupdated to setup socket.io and to serve a basic example.

--- muster_server/src/index.js
+++ muster_server/src/index.js
@@ -1,13 +1,31 @@
+import path from "path";
+import http from "http";
 import express from "express";
 import parser from "body-parser";
+import socket_io from "socket.io";
 import apiRoutes from "./routes/api";
 
 const app = new express();
-app.use(parser.urlencoded({extended: true}));
+const server = http.createServer(app);
+const io = socket_io(server);
+app.use(parser.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 
-app.get("/", (req, res) => {res.send("Root route")});
+const application_root_path = path.join(__dirname, "/../")
+const static_path = path.join(application_root_path, "/public")
 
-const port = process.env.PORT || 3000
-app.listen(port);
+app.use("/", express.static(static_path));
+
+const port = process.env.PORT || 3000;
+server.listen(port);
 console.log(`listening on port ${port}`);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    console.log(`message: ${msg}`);
+  });
+  socket.on("disconnect", () => {
+    console.log("a user has disconnected");
+  });
+});
