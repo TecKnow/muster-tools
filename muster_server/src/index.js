a5b2d2784a6f1841390268d65aaae0bbe0fad98c
David P. Perkins <tecknow@grumbleware.com>
2020-10-15 22:43:02 -0500

    Add API route

--- muster_server/src/index.js
+++ muster_server/src/index.js
@@ -1,10 +1,11 @@
-import http from 'http';
+import express from "express";
+import parser from "body-parser";
+import apiRoutes from "./routes/api";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello David!!\n');
-}).listen(1337, '127.0.0.1');
+const app = new express();
+app.use(parser.urlencoded({extended: true}));
+app.use("/api", apiRoutes);
 
-console.log('Server running at http://127.0.0.1:1337/');
-
-export default server;
+const port = process.env.PORT || 3000
+app.listen(port);
+console.log(`listening on port ${port}`);
