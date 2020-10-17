a5f7ca5f3de28733e22b82d87d93f7c6132bf5e5
David P. Perkins <tecknow@grumbleware.com>
2020-10-16 21:11:32 -0500

    Attach redux-socket.io middleware to the store
    The middleware currently sends all events to all clients.
    
    The client page now shows a history of received events.

--- muster_server/src/store/index.js
+++ muster_server/src/store/index.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSocketIoMiddleware from 'redux-socket.io'
 import logger from "redux-logger";
+import {io} from "../express-app";
+
+const socketIoMiddleware = createSocketIoMiddleware(io, "");
 
 import playersReducer from "./features/playersSlice";
 export default configureStore({
@@ -8,6 +12,7 @@ export default configureStore({
   },
   middleware: (getDefaultMiddleware) => {
     let res = getDefaultMiddleware()
+    res = res.concat(socketIoMiddleware);
     if(process.env.NODE_ENV === "development"){
       res = res.concat(logger);
     }
