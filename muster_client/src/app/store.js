0e313dd6bcb5b37f6880369401e3564bfc95f621
David P. Perkins <tecknow@grumbleware.com>
2021-04-10 16:16:44 -0500

    Copies the redux store to the client.
    Consider centralizing this if custom changes prove unneeded.

--- muster_client/src/app/store.js
+++ muster_client/src/app/store.js
@@ -3,12 +3,18 @@ import counterReducer from "../features/counter/counterSlice";
 import logger from "redux-logger";
 import createSocketIoMiddleware from "redux-socket.io";
 import socket from "../socket.io-config";
+import playersReducer from "./features/playersSlice";
+import tablesReducer from "./features/tablesSlice";
+import seatsReducer from "./features/seatsSlice";
 
 const socketIoMiddleware = createSocketIoMiddleware(socket, "server/");
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    players: playersReducer,
+    tables: tablesReducer,
+    Seats: seatsReducer,
   },
   middleware: (getDefaultMiddleware) => {
     let res = getDefaultMiddleware();
