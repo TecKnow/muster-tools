decb28141f71a0ad4a966f57fff8ec618bc0eca5
David P. Perkins <tecknow@grumbleware.com>
2021-04-17 14:05:43 -0500

    Switces layout to use Grid primarily

--- muster_client/src/Table.js
+++ muster_client/src/Table.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { makeStyles } from '@material-ui/core/styles';
-import { Chip, Paper, Typography } from "@material-ui/core";
+import { Chip, Paper, Typography, Grid } from "@material-ui/core";
 import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
 import { selectTableSeats } from "./features/seatsSlice";
 import { useSelector } from 'react-redux'
@@ -8,15 +8,7 @@ import { useSelector } from 'react-redux'
 const useStyles = makeStyles((theme) => ({
     playerListArea: {
         minHeight: theme.spacing(5),
-        minWidth: theme.spacing(15),
-        display: 'flex',
-        flexDirection: "column",
-        flexWrap: 'nowrap',
-        alignItems: "flex-start",
-        justifyItems: "flex-start",
-        '& > *': {
-            margin: theme.spacing(0.5)
-        },
+        padding: theme.spacing(1),
     },
 }));
 
@@ -29,14 +21,14 @@ const Table = ({ tableId }) => {
     const unsortedTableSeats = useSelector(tableSeatsSelectorFunc(tableId));
     const tableSeats = unsortedTableSeats.slice().sort((a, b) => a.position - b.position)
 
-    const listItems = tableSeats.map((seat) => (<Chip key={seat.id} label={seat.id} icon={<DragIndicatorIcon/>} className={classes.chip}/>));
+    const listItems = tableSeats.map((seat) => (<Grid item><Chip key={seat.id} label={seat.id} icon={<DragIndicatorIcon />} className={classes.chip} /></Grid>));
 
     return (
-        <Paper>
+        <Paper className={classes.paper}>
             <Typography>{`Table: ${tableId}`}</Typography>
-            <div className={classes.playerListArea}>
+            <Grid container direction="column" justify="flex-start" alignItems="flex-start" spacing={1} className={classes.playerListArea}>
                 {listItems}
-            </div>
+            </Grid>
         </Paper>
 
     );
--- muster_client/src/Tables.js
+++ muster_client/src/Tables.js
@@ -1,22 +1,18 @@
-import React from "react"
-import {useSelector} from "react-redux"
+import React from "react";
+import { useSelector } from "react-redux";
+import { Grid } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
-import {selectTableIds} from "./features/tablesSlice";
+import { selectTableIds } from "./features/tablesSlice";
 import Table from "./Table";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        display: 'flex',
-        flexDirection: "row",
-        flexWrap: 'wrap',
-        '& > *': {
-            
-        },
+        flexGrow: 1,
     },
 }));
 
 const TableSpread = (tableIds) => {
-    return tableIds.map(tableId => (<Table key={tableId} tableId={tableId}/>));
+    return tableIds.map(tableId => (<Grid item xs={12} sm={6} lg={3}><Table key={tableId} tableId={tableId} /></Grid>));
 }
 
 const Tables = () => {
@@ -24,7 +20,9 @@ const Tables = () => {
     const tableIds = useSelector(selectTableIds)
     return (
         <div className={classes.root}>
-            {TableSpread(tableIds)}
+            <Grid container spacing={3}>
+                {TableSpread(tableIds)}
+            </Grid>
         </div>
     );
 }
