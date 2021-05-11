2c838415604020d3ba1113f7b9cf056e72a0ed85
David P. Perkins <tecknow@grumbleware.com>
2021-05-10 22:52:40 -0500

    Removes redux serialization.
    This isn't needed since SQLite3 is being used.

--- muster_server/src/store/index.js
+++ muster_server/src/store/index.js
@@ -1,17 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import createSocketIoMiddleware from "redux-socket.io";
 import logger from "redux-logger";
-import { io } from "../express-app";
-import { store_writer, store_reader } from "./serialize_store";
 import {
   playersSlice,
   tablesSlice,
   seatsSlice,
 } from "@grumbleware/event-muster-store";
 
-const socketIoMiddleware = createSocketIoMiddleware(io, "");
-
-export const _makeStore = (initial_state = store_reader()) => {
+export const _makeStore = (initial_state) => {
   const preloadedState = initial_state;
   return configureStore({
     reducer: {
@@ -21,7 +16,6 @@ export const _makeStore = (initial_state = store_reader()) => {
     },
     middleware: (getDefaultMiddleware) => {
       let res = getDefaultMiddleware();
-      res = res.concat(socketIoMiddleware);
       if (process.env.NODE_ENV === "development") {
         res = res.concat(logger);
       }
